9e37ad3ad0bb522f817775bdaed92997bb40743c
rafael <yhrapha@gmail.com>
2024-04-12 23:06:29 +0100

    veriscan commit

--- src/Components/IsDeepfake/IsDeepfake.jsx
+++ src/Components/IsDeepfake/IsDeepfake.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react"
 import axios from 'axios'
 import {motion , AnimatePresence} from 'framer-motion'
-import { ArrowBackIosRounded, ErrorOutlineRounded} from "@mui/icons-material"
+import { ArrowBackIosRounded, ErrorOutlineRounded,WysiwygRounded, CloudDownloadRounded} from "@mui/icons-material"
 import { Link} from 'react-router-dom'
 import { BeatLoader} from 'react-spinners'
+import { usePDF } from 'react-to-pdf';
+
 import './isdeepfake.scss'
-import { dialogClasses } from "@mui/material"
 
 const IsDeepfake = () => {
 
@@ -16,8 +17,12 @@ const IsDeepfake = () => {
   const [msg, setMsg ]= useState('')
   const [error, setError ]= useState(false)
   const [isShown, setIsShown ]= useState(false)
+  const [array, setArray ]= useState([])
+  const [isOpen, setIsOpen ]= useState(false)
 
-
+  const { toPDF, targetRef } = usePDF({filename: 'image records.pdf', page:{
+    margin: 25
+  }});
 
   function readImageFile (file){
     return new Promise((resolve, reject)=>{
@@ -44,6 +49,9 @@ const IsDeepfake = () => {
     if(!file){
       return
     }
+    if(file.type === 'image/png' || file.type === 'image/webp' ){
+      return
+    }
     
     try {
       setIsLoading(true)
@@ -52,10 +60,19 @@ const IsDeepfake = () => {
       formData.append('file', file);
       setMsg('Analyzing image data')
       const response = await axios.post('https://veriscan-n.onrender.com/deepfake', formData)
-      setMsg('Making Predictions')
-      // console.log(response.data[0])
+
+      const [name ]= file.name.split('.')
+      const obj = { 
+        fileName: name,
+        fileType: file.type,
+        prediction: response.data[0].class,
+        accuracy: response.data[0].score * 100
+      }
+      setArray([...array, obj])
       setPredictions(response.data[0])
+      // console.log(array)
       setIsLoading(false)
+
     } catch (error) {
       console.log(error)
       setIsLoading(false)
@@ -104,7 +121,7 @@ const IsDeepfake = () => {
             does not support .png and .webp files
           </i>
           <input type="file" onChange={HANDLECHANGE}/>
-          <button onClick={HANDLEUPLOAD}>
+          <button onClick={HANDLEUPLOAD} disabled={isLoading}>
             Scan Image Data
           </button>
         </div>
@@ -166,6 +183,44 @@ const IsDeepfake = () => {
 
           </div>
         </div>)}
+
+        {array && array.length > 0 && <motion.div className="mt"
+            initial={{opacity:0}}
+            animate={{opacity:1}}
+            transition={{duration:.5}}>
+          <abbr title="show recent image records">
+            <WysiwygRounded onClick={()=> setIsOpen(!isOpen)}/>
+          </abbr>    
+        </motion.div>}
+      
+        <AnimatePresence>
+          {array && array.length > 0 && isOpen && (<motion.div ref={targetRef} className="records"
+          initial={{opacity:0}}
+          animate={{opacity:1}}
+          transition={{duration:.5}}
+          exit={{opacity:0}}>
+            
+            <p>IMAGE RECORDS</p>
+
+            {array.map((result, i)=>{
+              return (
+              <div key={i} className="record">
+                <i>{i+1}.</i>
+                <p>name: <span>{result.fileName}</span></p>
+                <p>file type: <span>{result.fileType}</span></p>
+                <p>prediction: <span style={{color: result.prediction === 'Deepfake' ? 'red' : 'green'}}>{result.prediction.toUpperCase()}</span></p>
+                <p>accuracy: <span>{Math.round(result.accuracy)}%</span></p>  
+              </div>
+              )
+            })}
+
+            <motion.div whileHover={{scale:.9}}>
+              <CloudDownloadRounded onClick={() => toPDF()} className="mt"/>
+            </motion.div>
+            
+          </motion.div>)}
+        </AnimatePresence>
+
       </motion.div>
     </div>
   )
